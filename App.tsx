3a1439f864653ff42e8c6463a8e543f8a4943754
Tdoval <thiagodovalaraujo@gmail.com>
2021-06-09 11:41:40 -0300

    header component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
 
-export default function App() {
+const App = () => {
+
+  const [colors, setColors] = useState([
+    {id: '1', name: 'color1', hex: '#04429A', image:"http://www.thecolorapi.com/id?format=svg&named=false&hex=04429A"},
+    {id: '2', name: 'Blue Gray', hex: '#0309A2', image:"http://www.thecolorapi.com/id?format=svg&named=false&hex=0309A2"},
+    {id: '3', name: 'Kingfisher Daisy', hex: '#3A02A9', image:"http://www.thecolorapi.com/id?format=svg&named=false&hex=3A02A9"},
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <Header />
+      <Text>Txto</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex:1,
+    paddingTop: 40
+  }
+})
+
+export default App;
---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+
+const Header = () => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.text}>Ton</Text>
+            <AntDesign name="shoppingcart" size={28} color="black" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        padding: 15,
+        backgroundColor:'#10c662',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    text: {
+        color: 'black',
+        fontSize:23,
+    }
+})
+
+export default Header;
